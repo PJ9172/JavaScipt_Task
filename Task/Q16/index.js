9c8314c10f16a34d2026ac485dffba9451c76acd
Prajwal <rohitjagatap5000@gmail.com>
2024-01-18 17:57:22 +0530

    Q16 updated

--- Task/Q16/index.js
+++ Task/Q16/index.js
@@ -1,3 +1,8 @@
+var h1=document.getElementById('1');
+var h2=document.getElementById('2');
+var h3=document.getElementById('3');
+var h4=document.getElementById('4');
+var h5=document.getElementById('5');
 var url="https://api.github.com/users/PJ9172";
 async function fun()
 {
@@ -5,11 +10,11 @@ async function fun()
         var res=await fetch(url);
         // console.log(res);
         var result=await res.json();
-        console.log(result.name);
-        console.log(result.login);
-        console.log(result.id);
-        console.log(result.url);
-        console.log(result.following);
+        h1.innerHTML=`<h1>name : ${result.name}</h1>`;
+        h2.innerHTML=`<h1>login : ${result.login}</h1`;
+        h3.innerHTML=`<h1>id : ${result.id}</h1>`;
+        h4.innerHTML=`<h1>url : ${result.url}</h1>`;
+        h5.innerHTML=`<h1>following : ${result.following}</h1>`;
     }
     catch(err){
         console.log(err);
